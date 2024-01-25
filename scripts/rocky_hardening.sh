d89d9f1e71282169a2b3e3771771fead07d7a2e1
wakeupdanila <skinny4550@gmail.com>
2024-01-25 13:49:36 +0100

    update repo structure

--- scripts/rocky_hardening.sh
+++ scripts/rocky_hardening.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
-# Pobieranie aktualizacji systemu 
+source utils.sh
+
+# Update the system
+info "Checking for security updates available..."
 yum update -qy --security
+info "Security updates checked"
 
-# Konfiguracja dostępu do plików i katalogów
-chmod 755 /path/to/directory
-
-# Konfiguracja umask
+# Configure umask
 echo "umask 027" >> /etc/profile
 
-# Sprawdzanie jakości haseł i polityki haseł
+# Check password quality and password policy
 sed -i 's/password requisite pam_pwquality.so try_first_pass local_users_only/password requisite pam_passwdqc.so min=disabled,disabled,16,12,8/' /etc/security/pwquality.conf
 sed -i 's/password requisite pam_pwquality.so try_first_pass local_users_only/password requisite pam_passwdqc.so min=disabled,disabled,16,12,8/' /etc/pam.d/system-auth
 
-# Konfiguracja SELinux
+# Configure SELinux
 setenforce 1
 sed -i 's/SELINUX=permissive/SELINUX=enforcing/g' /etc/selinux/config
 
-# Konfiguracja firewalla (firewall-cmd)
+# Configure firewall (firewall-cmd)
 firewall-cmd --zone=public --add-port=22/tcp --permanent
 firewall-cmd --zone=public --add-masquerade --permanent
 firewall-cmd --zone=public --add-drop --permanent
 firewall-cmd --reload
 
-# Konfiguracja SSH
-backup_files /etc/ssh/sshd_config
+# Configure SSH
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 
-# Konfiguracja auditd
-#echo "-w /etc/passwd -p wa -k passwd_changes" >> /etc/audit/audit.rules
-#systemctl restart auditd
+# Configure auditd
+echo "-w /etc/passwd -p wa -k passwd_changes" >> /etc/audit/audit.rules
+systemctl restart auditd
 
-# Konfiguracja Apache HTTP server
+# Configure Apache HTTP server
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-
-echo "Skrypt hardeningowy zakończony."
