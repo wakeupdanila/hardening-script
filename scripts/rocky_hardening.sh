dbe3558919815e10829e9767c31abe7246321cc5
wakeupdanila <skinny4550@gmail.com>
2024-01-25 15:41:13 +0100

    testing

--- scripts/rocky_hardening.sh
+++ scripts/rocky_hardening.sh
@@ -28,14 +28,14 @@ else
     warn "Pattern $pattern not found in $file. Skipping sed command."
 fi
 
-# Configure SELinux
-pattern="SELINUX=disabled"
-file="/etc/selinux/config"
-if grep -q "$pattern" "$file"; then
-    perform_action "Configure SELinux" "sed -i 's/SELINUX=disabled/SELINUX=enforcing/' $file"
-else
-    warn "Pattern $pattern not found in $file. Skipping sed command."
-fi
+# Configure SELinux [!!!]
+#pattern="SELINUX=disabled"
+#file="/etc/selinux/config"
+#if grep -q "$pattern" "$file"; then
+#    perform_action "Configure SELinux" "sed -i 's/SELINUX=disabled/SELINUX=enforcing/' $file"
+#else
+#    warn "Pattern $pattern not found in $file. Skipping sed command."
+#fi
 
 # Configure firewall (firewall-cmd)
 perform_action "Configure firewall" "firewall-cmd --permanent --add-service=http"
@@ -54,8 +54,8 @@ else
     warn "Pattern $pattern not found in $file. Skipping sed command."
 fi
 
-# Configure SSH ciphers
-perform_action "Configure SSH ciphers" "sed -i '$a Ciphers aes256-gcm@openssh.com,aes128-gcm@openssh.com,chacha20-poly1305@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr' /etc/ssh/sshd_config"
+# Configure SSH ciphers [!!!]
+#perform_action "Configure SSH ciphers" "sed -i '$a Ciphers aes256-gcm@openssh.com,aes128-gcm@openssh.com,chacha20-poly1305@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr' /etc/ssh/sshd_config"
 
 # Configure auditd
 perform_action "Configure auditd" "echo '-w /etc/passwd -p wa -k identity' >> /etc/audit/rules.d/audit.rules"
@@ -83,18 +83,20 @@ else
     warn "Pattern $pattern not found in $file. Skipping sed command."
 fi
 
-# Enable Automatic Security Updates
-perform_action "Enable Automatic Security Updates" "yum install -y yum-cron"
-perform_action "Configure Automatic Security Updates" "sed -i 's/apply_updates = no/apply_updates = yes/' /etc/yum/yum-cron.conf"
-perform_action "Start Automatic Security Updates" "systemctl start yum-cron"
-perform_action "Enable Automatic Security Updates at boot" "systemctl enable yum-cron"
+# Enable Automatic Security Updates [!!!]
+#perform_action "Enable Automatic Security Updates" "yum install -y yum-cron"
+#perform_action "Configure Automatic Security Updates" "sed -i 's/apply_updates = no/apply_updates = yes/' /etc/yum/yum-cron.conf"
+#perform_action "Start Automatic Security Updates" "systemctl start yum-cron"
+#perform_action "Enable Automatic Security Updates at boot" "systemctl enable yum-cron"
 
-# Install and Configure Fail2Ban
-perform_action "Install Fail2Ban" "yum install -y fail2ban"
-perform_action "Start Fail2Ban" "systemctl start fail2ban"
-perform_action "Enable Fail2Ban at boot" "systemctl enable fail2ban"
+# Install and Configure Fail2Ban [!!!]
+#perform_action "Install Fail2Ban" "yum install -y fail2ban"
+#perform_action "Start Fail2Ban" "systemctl start fail2ban"
+#perform_action "Enable Fail2Ban at boot" "systemctl enable fail2ban"
 
 # Install and Configure AIDE
 perform_action "Install AIDE" "yum install -y aide"
 perform_action "Initialize AIDE" "aide --init"
 perform_action "Configure AIDE" "mv /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz"
+
+info "System hardening completed successfully"
